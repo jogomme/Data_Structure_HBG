3fd782ac16ecce5a32fe1428bde45365e3cc487e
jogomme <beomgih945@gmail.com>
2026-06-04 12:40:23 +0900

    해싱
    해싱은 Python의 dictionary 와 비슷한 자료구조를 가진다
    
    Key와 value를 가지며 key값을 주면 value를 return해주는 것이 해싱이다.
    
    해싱은 add, delete, search 기능을 가진다.
    
    해싱은 특정 key값을 주면 '해싱 함수'에 그 key값을 넘겨주면 헤시 테이블의 주소 중 하나를 준다.
    
    이때 해시 함수는 서로 다른 키들로 서로 다른 주소값을 도출해야하며 충돌이 적거나 없더라도 주소값을 도출하는데 시간이 너무 오래 걸리면 안된다.
    
    해시 테이블에서의 충돌이란 이미 그 주소에 데이터가 있는데 key값을 주었더니 똑같은 주소를 도출해내는 것이다.
    
    이때 이것을 방지하기 위한 2개의 기법이 있다
    
    첫 번째 방법은 선형조사법으로
    선형 조사법은 충돌이 일어나면 다음 주소값에 저장을 하는 것이다.
    하지만 이 방법을 쓰면 데이터의 군집화와 결합이 일어날 수 있는데 쉽게 풀어쓰자면 충돌이 일어난 곳 주변에 계속해서 충돌이 일어나는 것이다. 이를 해결하기 위한 대표적인 방법 중 하나는 더블 해싱 방법이고 나는 이 코드를 사용하였다.
    
    두 번째 방법은 체이닝 기법이다
    체이닝 기법은 주소값을 넘겨주고 거기서 연결리스트를 활용하여 주는 방법이다.
    
    두 개의 방법은 약간의 차이가 있는데
    선형조사법은 제적율( 들어온 데이터 의 수 / 메모리의 수) 이 높아지면 높아질수록 충돌 발생율이 늘어나 속도가 급격하게 줄어든다, 그럼으로 제적율이 0.5 이상으로 안가게 유지를 해야하며 이는 사용가능한 메모리가 전체의 절반 이하임으로 메모리가 부족한 상황에서는 쓰이지 않는다.
    체이싱은 반면에 메모리 공간의 한계는 이론상은 존제하지 않는다 하지만 선형분석법은 배열을 사용해서 연속된 데이터 공간이고 체이싱 기법은 연결리스트로 분리된 데이터 공간이니 제적율이 충분히 낮다면 선형 분석법이 더 빠를 수 있다. 또한 체이싱의 제적률은 1 이상으로 올라갈 수 있다.

---
+++ 실습과제/해싱/Proj_20_HBG/Proj_20_HBG.cpp
@@ -0,0 +1,225 @@
+﻿#include <stdlib.h>
+#include<iostream>
+#include<string>
+
+#define TABLE_SIZE 10000 // 가능하면 해싱 테이블의 크기를 소수로 설정 
+#define SIZE TABLE_SIZE/2 //적재율을 SIZE/TABLE_SIZE로 계산
+#define SEARCH_COUNT 1000000
+
+#define INF 999999
+
+typedef struct {
+	int key;
+} element;
+
+struct list
+{
+	element item;
+	struct list* link;
+};
+
+//struct list* Chash_table[TABLE_SIZE]; 체이닝 기법
+struct list** Chash_table; 
+
+//element Lhash_table[TABLE_SIZE]; 선형기법
+element* Lhash_table; 
+
+// 선형 해쉬 테이블 초기화
+void init_Lhash_table(element Lhash_table[], int TableSize)
+{
+	for (int i = 0; i < TableSize; ++i) {
+		Lhash_table[i].key = INF;
+	}
+}
+
+// 체이스 해쉬 테이블 초기화
+void init_Chash_table(struct list* Chash_table[], int TableSize)
+{
+	for (int i = 0; i < TableSize; ++i) {
+		Chash_table[i] = nullptr;
+	}
+}
+
+
+// 소수인지 판별하는 방법
+bool isPrime(int num)
+{
+	for (int i = 2; i * i <= num; ++i) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// TableSize보다 작은 최대 소수 구하기
+int get_prime_number(int TableSize)
+{
+	// 1이하면 소수가 없음
+	if (TableSize <= 1) {
+		return -1;
+	}
+
+
+	for (int i = TableSize - 1; i >= 2; --i) {
+		if (isPrime(i)) {
+			return i;
+		}
+	}
+
+	return -1;
+
+}
+
+// 제산 함수를 사용한 해싱 함수
+int hash_function(int key)
+{
+	return key % TABLE_SIZE;
+}
+
+// 이중해시를 도입한 선형적 기법 해시 삽입
+void hash_linear_add(element item, element Lhash_table[], int hPrime)
+{
+	int hash_index;
+	int value;
+
+	hash_index = value = hash_function(item.key);
+
+	int doubleHash = hPrime - (item.key % hPrime);
+
+	do {
+		if (Lhash_table[hash_index].key == INF) {
+			Lhash_table[hash_index] = item;
+			return;
+		}
+
+		hash_index = (hash_index + doubleHash)%TABLE_SIZE;
+
+
+
+	} while (value != hash_index);
+
+	std::cout << "해시 포화" << '\n';
+
+}
+
+// 선형적 기법을 이용한 해시 탐색
+element hash_linear_search(element item, element Lhash_table[], int hPrime)
+{
+	int hash_index;
+	int value;
+
+	hash_index = value = hash_function(item.key);
+
+	int doubleHash = hPrime - (item.key % hPrime);
+
+	do {
+		if (Lhash_table[hash_index].key == item.key) {
+			//std::cout << item.key << "탐색 위치 " << hash_index << '\n';
+			return Lhash_table[hash_index];
+		}
+
+		hash_index = (hash_index + doubleHash) % TABLE_SIZE;
+
+		if (Lhash_table[hash_index].key == INF) {
+			break;
+		}
+
+	} while (value != hash_index);
+
+	element e;
+	e.key = INF;
+
+	//std::cout << "찾는 값이 테이블에 없습니다.";
+	return e;
+}
+
+// 체이닝 기법을 활용한 해시 삽입
+void hash_chain_add(element item, list* Chash_table[])
+{
+	int hash_index;
+
+	hash_index =  hash_function(item.key);
+
+	list* newNode = new list;
+
+	newNode->item = item;
+	
+	newNode->link = Chash_table[hash_index];
+
+	Chash_table[hash_index] = newNode;
+
+}
+
+element hash_chain_search(element item, list* Chash_table[])
+{
+	int hash_index;
+
+	hash_index = hash_function(item.key);
+
+	list* CurNode = Chash_table[hash_index];
+
+	while (CurNode != nullptr) {
+		if (CurNode->item.key == item.key) {
+			//std::cout << item.key << " 탐색 위치 : " << hash_index << '\n';
+			return CurNode->item;
+		}
+		CurNode = CurNode->link;
+	}
+
+	element e;
+	e.key = INF;
+
+	//std::cout << "데이터에 해당 키 값을 지닌 데이터는 없습니다" << '\n';
+
+	return e;
+
+}
+
+
+int main(void)
+{
+	int s_time, e_time;
+	int h_prime;
+	element e;
+	Lhash_table = (element*)malloc(sizeof(element) * TABLE_SIZE);
+	Chash_table = (struct list**)malloc(sizeof(struct list*) * TABLE_SIZE);
+	init_Lhash_table(Lhash_table, TABLE_SIZE);
+	init_Chash_table(Chash_table, TABLE_SIZE);
+	srand(100);
+
+	//이중 해시법을 적용하기 위한 버킷 사이즈보다 작은 가장 큰 소수 구하기
+	h_prime = get_prime_number(TABLE_SIZE); 
+
+	for (int i = 0; i < SIZE; i++) { //적재율까지만 저장
+		e.key = rand();
+		hash_linear_add(e, Lhash_table, h_prime);
+		hash_chain_add(e, Chash_table);
+	}
+
+	srand(200);
+	s_time = clock();
+	for (int i = 0; i < SEARCH_COUNT; i++) {
+		e.key = rand();
+		hash_linear_search(e, Lhash_table, h_prime);
+	}
+	e_time = clock();
+
+	printf("선형기법\n");
+	std::cout << "적재율이 " << (float)SIZE / TABLE_SIZE << "인경우 " << SEARCH_COUNT << "회 검색 시 소요시간 "
+		<< e_time - s_time << '\n';
+
+	srand(200);
+	s_time = clock();
+	for (int i = 0; i < SEARCH_COUNT; i++) {
+		e.key = rand();
+		hash_chain_search(e, Chash_table);
+	}
+	e_time = clock();
+	printf("체인기법\n");
+
+	std::cout << "적재율이 " << (float)SIZE / TABLE_SIZE << "인경우 " << SEARCH_COUNT << "회 검색 시 소요시간 "
+		<< e_time - s_time << '\n';
+
+	return 0;
+}
